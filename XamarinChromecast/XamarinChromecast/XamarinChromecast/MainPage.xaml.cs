f9f37a2e49157a4b00447800e4ecf50c1dd8d719
Jorge Diego <jorge@devsdna.com>
2020-11-10 06:41:51 +0100

    Improvements on devices list and playing video

--- XamarinChromecast/XamarinChromecast/XamarinChromecast/MainPage.xaml.cs
+++ XamarinChromecast/XamarinChromecast/XamarinChromecast/MainPage.xaml.cs
@@ -1,5 +1,10 @@
-﻿using System;
+﻿using GoogleCast;
+using GoogleCast.Channels;
+using GoogleCast.Models.Media;
+using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Forms.PlatformConfiguration;
 using Xamarin.Forms.PlatformConfiguration.iOSSpecific;
@@ -8,10 +13,12 @@ namespace XamarinChromecast
 {
     public partial class MainPage : ContentPage
     {
-        public List<string> ChromecastDevices { get; set; }
+        private readonly DeviceLocator deviceLocator;
+        private List<IReceiver> chromecastDevices;
 
         public MainPage()
         {
+            deviceLocator = new DeviceLocator();
             InitializeComponent();
         }
 
@@ -21,16 +28,56 @@ namespace XamarinChromecast
             FrChromecastDevices.TranslationY = DeviceDisplayInfo.ScreenHeight + 5;
         }
 
-        private void BtCast_Clicked(object sender, EventArgs e)
+        private async void BtCast_Clicked(object sender, EventArgs e)
         {
-            ChromecastDevices = new List<string> { "Device1", "Device2" };
-            CvChromecastDevices.ItemsSource = ChromecastDevices;
+            await LoadReceivers();
 
             FrChromecastDevices.IsVisible = true;
-            FrChromecastDevices.TranslateTo(
+            await FrChromecastDevices.TranslateTo(
                 0, 
-                DeviceDisplayInfo.ScreenHeight - (ChromecastDevices.Count * 50) - On<iOS>().SafeAreaInsets().Bottom - FrChromecastDevices.Padding.VerticalThickness - 50, 
+                DeviceDisplayInfo.ScreenHeight - (chromecastDevices.Count * 50) - On<iOS>().SafeAreaInsets().Bottom - FrChromecastDevices.Padding.VerticalThickness - 100, 
                 250);
+        }
+
+        private async Task LoadReceivers()
+        {
+            chromecastDevices = null;
+            chromecastDevices = (await new DeviceLocator().FindReceiversAsync()).ToList();
+            CvChromecastDevices.ItemsSource = chromecastDevices;
+        }
+
+        private async void CvChromecastDevices_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            await PlayVideo();
+        }
+
+        private async Task PlayVideo()
+        {
+            IReceiver selectedCastDevice = CvChromecastDevices.SelectedItem as IReceiver;
+            if (selectedCastDevice == null)
+                return;
+
+            Sender ccSender = new Sender();
+            await ccSender.ConnectAsync(selectedCastDevice);
+
+            IMediaChannel mediaChannel = ccSender.GetChannel<IMediaChannel>();
+            await ccSender.LaunchAsync(mediaChannel);
+
+            MediaStatus mediaStatus = await mediaChannel.LoadAsync(new MediaInformation() { ContentId = EnUrl.Text });
+
+            CvChromecastDevices.SelectedItem = null;
+            await CloseReceiversList();
+        }
+
+        private async void BtClose_Clicked(object sender, EventArgs e)
+        {
+            await CloseReceiversList();
+        }
+
+        private async Task CloseReceiversList()
+        {
+            await FrChromecastDevices.TranslateTo(0, DeviceDisplayInfo.ScreenHeight + 5, 250);
+            FrChromecastDevices.IsVisible = false;
         }
     }
 }
